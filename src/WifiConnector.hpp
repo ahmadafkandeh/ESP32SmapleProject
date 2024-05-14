e99b4eab9b219c4edcd81e3d901e19edd7686fa6
Ahmad Afkande <ahamdafkandeh@gmail.com>
2024-05-14 15:39:26 +0200

    KAN-4 WiFi connector class now has connect form with saving the credentials auto connect to first available saved WiFi retrieves saved WiFis at constructor has a thread to check for saved wifi connections and updating wifi status

--- src/WifiConnector.hpp
+++ src/WifiConnector.hpp
@@ -1,32 +1,58 @@
 #ifndef __WIFI_CONNECTOR_H_
 #define __WIFI_CONNECTOR_H_
 
+#include <Arduino.h>
 #include <lvgl.h>
+#include <list>
+#include <mutex>
 
+struct SSIDStruct
+{
+    String ssid;
+    String EncryptedPassword;
+};
 class WIFIConnector
 {
     public:
     WIFIConnector(lv_obj_t *parent = NULL);
     ~WIFIConnector();
     void show();
-    void connect();
+
 protected:
+    bool connect();
+    bool isSelectedAPOpen();
+    void scanNetworks();
     void updateList();
     void getCurrent();
     void clearList();
+    void saveCredentials();
+    void loadAllSavedNetworks();
+    void ConnectToFirstAvailable();
+    void ForgetNetwork(String ssid);
+    static void WiFiTask(void* params);
+    String encryptPassword(String rawPass);
+    String DecryptPassword(String encpass);
 
-private:
+protected:
     lv_obj_t * wifiList;
     lv_obj_t * window;
     lv_obj_t * connectWindow;
-    lv_obj_t * lblSSID;
-    lv_obj_t * lblSSIDName;
     lv_obj_t * lblPassword;
     lv_obj_t * txtPassword;
     lv_obj_t * chkbxAutoReconnect; 
     lv_obj_t * btnCancel;
     lv_obj_t * btnConnect_Done;
     lv_obj_t * btnRefresh;
+    lv_obj_t * mainWindowTitle;
+    lv_obj_t * connectWindowTitle;
+    lv_obj_t * btnClose;
+    lv_obj_t * btnSavedAPs;
+    String selectedSSID; 
+    int foundNetworks;
+    SemaphoreHandle_t _mutex;
+    TaskHandle_t wifiTaskHandler;
+    std::list<SSIDStruct> ssids;
+
 };
 
 #endif // !__WIFI_CONNECTOR_H_
